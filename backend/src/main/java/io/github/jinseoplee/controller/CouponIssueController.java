d2c6023021d26e095e69b3e69e9c664030794fde
jinseoplee <jjjp9966@gmail.com>
2026-04-10 23:53:15 +0900

    feat: 쿠폰 발급 API 추가

---
+++ backend/src/main/java/io/github/jinseoplee/controller/CouponIssueController.java
@@ -0,0 +1,22 @@
+package io.github.jinseoplee.controller;
+
+import io.github.jinseoplee.service.CouponIssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/coupons")
+@RequiredArgsConstructor
+public class CouponIssueController {
+
+    private final CouponIssueService couponIssueService;
+
+    @PostMapping("/{couponId}/issue")
+    public ResponseEntity<Void> issue(@PathVariable(name = "couponId") Long couponId,
+                                      @RequestHeader("X-User-Id") Long userId) {
+        couponIssueService.issue(couponId, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
