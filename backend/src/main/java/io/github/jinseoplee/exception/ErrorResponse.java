ed3f335b42a5af82dae338025c93278712a2cfaa
jinseoplee <jjjp9966@gmail.com>
2026-04-10 23:48:29 +0900

    feat: ErrorResponse 및 GlobalExceptionHandler 추가

---
+++ backend/src/main/java/io/github/jinseoplee/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package io.github.jinseoplee.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    private ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage()
+        );
+    }
+}
---
+++ backend/src/main/java/io/github/jinseoplee/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package io.github.jinseoplee.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unhandled Exception occurred: ", e);
+
+        return ResponseEntity
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+                .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR));
+    }
+}
